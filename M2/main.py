f3b19de15e8a0bbab44000ba819cc84180de4125
OrbyDoll <antonkhazin2006@gmail.com>
2025-09-17 18:26:41 +0300

    collision v1

--- M2/main.py
+++ M2/main.py
@@ -1,15 +1,22 @@
 import pygame as pg
 from typing import List
+import math
 
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-TABLE_WIDTH = 800
+TABLE_WIDTH = 1000
 TABLE_HEIGHT = 600
 MARGIN_TOP = (SCREEN_HEIGHT - TABLE_HEIGHT) / 2
 MARGIN_LEFT = (SCREEN_WIDTH - TABLE_WIDTH) / 2
 BORDER_RADIUS = 30
+BORDER = 50
+BORDER_WIDTH = TABLE_WIDTH + BORDER
+BORDER_HEIGHT = TABLE_HEIGHT + BORDER
+BORDER_MARGIN_TOP = MARGIN_TOP - BORDER / 2
+BORDER_MARGIN_LEFT = MARGIN_LEFT - BORDER / 2
+BORDER_BORDER_RADIUS = int(BORDER_RADIUS + BORDER / 2)
 BALL_SIZE = 15
-COLORS = {"GREEN" : (0, 100, 0)}
+COLORS = {"GREEN" : (0, 100, 0), "BROWN": (101,67,33)}
 
 class Vector:
     def __init__(self, x: float, y: float):
@@ -21,6 +28,9 @@ class Vector:
     
     def __mul__(self, scalar: float):
         return Vector(self.x * scalar, self.y * scalar)
+    
+    def module(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
     
     def dot(self):
         return (self.x, self.y)
@@ -35,6 +45,10 @@ class Ball:
 
     def update(self, dt: float):
         self.position += self.velocity * dt
+        if self.velocity.module() > 0:
+            if self.check_collision():
+                print("Collision")
+                self.velocity = Vector(0,0)
 
     def draw(self, table):
         pg.draw.circle(table, self.color, self.position.dot(), self.radius)
@@ -43,11 +57,20 @@ class Ball:
         if (dot.x - self.position.x) ** 2 + (dot.y - self.position.y) ** 2 < self.radius ** 2:
             return True
         return False
+    
+    def check_collision(self):
+        return (
+            self.position.x - self.radius <= MARGIN_LEFT or
+            self.position.x + self.radius >= SCREEN_WIDTH - MARGIN_LEFT or
+            self.position.y + self.radius >= SCREEN_HEIGHT - MARGIN_TOP or
+            self.position.y - self.radius <= MARGIN_TOP
+        )
 
 class Game:
     def __init__(self):
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.table = pg.draw.rect(self.screen, COLORS["GREEN"], (MARGIN_LEFT, MARGIN_TOP, TABLE_WIDTH, TABLE_HEIGHT), border_radius=BORDER_RADIUS)
+        pg.draw.rect(self.screen, COLORS["BROWN"], (BORDER_MARGIN_LEFT, BORDER_MARGIN_TOP, BORDER_WIDTH, BORDER_HEIGHT), border_radius=BORDER_BORDER_RADIUS)
         self.balls: List[Ball] = []
         self.dt = pg.time.Clock().tick(60) / 1000.0
         self.running = True
@@ -60,13 +83,11 @@ class Game:
             self.handle_events()
             self.update()
             self.draw()
-
             
     def draw(self):
         self.table = pg.draw.rect(self.screen, COLORS["GREEN"], (MARGIN_LEFT, MARGIN_TOP, TABLE_WIDTH, TABLE_HEIGHT), border_radius=BORDER_RADIUS)
         for ball in self.balls:
             ball.draw(self.screen)
-
         pg.display.flip()
 
     def update(self):
@@ -80,13 +101,16 @@ class Game:
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_LALT:
                     self.mode = "create" if self.mode == "game" else "game"
+                elif event.key == pg.K_q:
+                    self.running = False
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     cursor_pos = pg.mouse.get_pos()
                     if self.mode == "game":
                         print("Play")
                         current_ball: Ball = self.get_ball_by_coord(Vector(cursor_pos[0], cursor_pos[1]))
-                        current_ball.velocity = Vector(2, 1)
+                        if current_ball != None:
+                            current_ball.velocity = Vector(-5, 0)
                     else:
                         print("Create")
                         ball = Ball(Vector(cursor_pos[0], cursor_pos[1]))
@@ -96,6 +120,8 @@ class Game:
         for ball in self.balls:
             if ball.check_dot(position):
                 return ball
+        return None
+    
 if __name__ == "__main__":
     game = Game()
     game.run()
