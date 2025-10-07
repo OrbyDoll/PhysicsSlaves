844ef8953ccc3fdc77c714efa45cec6d224ced82
OrbyDoll <antonkhazin2006@gmail.com>
2025-10-07 17:54:54 +0300

    added force appliement

--- M2/main.py
+++ M2/main.py
@@ -9,7 +9,7 @@ TABLE_COEF = 0.7
 TABLE_WIDTH = SCREEN_WIDTH * TABLE_COEF 
 TABLE_HEIGHT = SCREEN_HEIGHT * TABLE_COEF
 CUE_LENGTH = 300
-CUE_WIDTH = 7
+CUE_WIDTH = 5
 MARGIN_TOP = (SCREEN_HEIGHT - TABLE_HEIGHT) / 2
 MARGIN_LEFT = (SCREEN_WIDTH - TABLE_WIDTH) / 2
 BORDER_RADIUS = 30
@@ -43,6 +43,14 @@ class Vector:
     
     def dot(self):
         return (self.x, self.y)
+    
+    def get_normal(self):
+        return Vector(-self.y, self.x)
+    
+    def normalize(self):
+        if (self.module() > 0):
+            return self * (1 / self.module())
+        return Vector(0, 1)
 
 
 class Ball:
@@ -88,13 +96,23 @@ class Cue:
         self.position = CUE_POLYGON
 
     def draw(self):
-        pg.draw.line(self.table, COLORS["CUE"], self.position[:2], self.position[2:])
+        start_vector = Vector(self.position[0], self.position[1])
+        end_vector = Vector(self.position[2], self.position[3])
+
+        normal_vector = (start_vector - end_vector).get_normal().normalize()
+        vertexes = []
+
+        vertexes.append((start_vector + normal_vector * (CUE_WIDTH / 2)).dot())
+        vertexes.append((start_vector - normal_vector * (CUE_WIDTH / 2)).dot())
+        vertexes.append((end_vector - normal_vector * (CUE_WIDTH / 2)).dot())
+        vertexes.append((end_vector + normal_vector * (CUE_WIDTH / 2)).dot())
+
+        pg.draw.polygon(self.table, COLORS["CUE"], vertexes)
 
     def update(self):
         if (self.is_aimed):
             self.follow_cursor()
         
-
     def aim_cue(self, aimed_ball: Ball):
         self.is_aimed = not self.is_aimed
         self.aimed_ball = aimed_ball 
@@ -104,23 +122,30 @@ class Cue:
         cursor_vector = Vector(cursor_pos[0], cursor_pos[1])
         ball_vector = self.aimed_ball.position
 
-        try:
-            line_koef = (cursor_vector.y - ball_vector.y) / (cursor_vector.x - ball_vector.x)
-        except:
-            line_koef = 0
-        shift1 = 3 * self.aimed_ball.radius
+        x_dist = cursor_vector.x - ball_vector.x
+        y_dist = cursor_vector.y - ball_vector.y
+        shift1 = 2 * self.aimed_ball.radius
         shift2 = shift1 + CUE_LENGTH
+        if x_dist != 0:
+            line_koef = y_dist / x_dist
 
-        x1_shift = shift1 / np.sqrt(1 + line_koef ** 2)
-        y1_shift = x1_shift * line_koef
+            x1_shift = shift1 / np.sqrt(1 + line_koef ** 2)
+            y1_shift = x1_shift * line_koef
 
-        x2_shift = shift2 / np.sqrt(1 + line_koef ** 2)
-        y2_shift = x2_shift * line_koef
-
-        start_pos = ball_vector - Vector(x1_shift, y1_shift)
-        end_pos = ball_vector - Vector(x2_shift, y2_shift)
+            x2_shift = shift2 / np.sqrt(1 + line_koef ** 2)
+            y2_shift = x2_shift * line_koef
+            start_pos = ball_vector + Vector(x1_shift , y1_shift) * np.sign(x_dist)
+            end_pos = ball_vector + Vector(x2_shift, y2_shift) * np.sign(x_dist)
+        else:
+            start_pos = ball_vector + Vector(0, shift1) * np.sign(y_dist)
+            end_pos = ball_vector + Vector(0, shift2) * np.sign(y_dist)
 
         self.position = [*start_pos.dot(), *end_pos.dot()]
+
+    def apply_force(self, force: int):
+        if self.aimed_ball:
+            aim_vector = Vector(self.position[0] - self.position[2], self.position[1] - self.position[3]).normalize() * force
+            self.aimed_ball.velocity = aim_vector
 
 class Game:
     def __init__(self):
@@ -130,7 +155,7 @@ class Game:
         self.border = pg.draw.rect(self.screen, COLORS["BROWN"], (BORDER_MARGIN_LEFT, BORDER_MARGIN_TOP, BORDER_WIDTH, BORDER_HEIGHT), border_radius=BORDER_BORDER_RADIUS)
         self.balls: List[Ball] = []
         self.dt = pg.time.Clock().tick(60) / 1000.0
-        self.cue_pos = Vector(0, 0)
+        self.hit_power = ""
         self.running = True
         self.mode = "game"
 
@@ -163,19 +188,34 @@ class Game:
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_LALT:
                     self.mode = "create" if self.mode == "game" else "game"
+                elif event.key == pg.K_LCTRL:
+                    self.mode = "velocity" if self.mode != "velocity" else "aimed" if self.cue.aimed_ball else "game"
                 elif event.key == pg.K_q:
                     self.running = False
+                elif self.mode == "velocity":
+                    if event.key in range(48, 58):
+                        self.hit_power += chr(int(event.key))
+                        print(f"Current force is {self.hit_power}")
+                    elif event.key == 8:
+                        self.hit_power = self.hit_power[:-1]
+                        print(f"Current force is {self.hit_power}")
+                elif self.mode == "aimed" and event.key == pg.K_SPACE:
+                    if len(self.hit_power) > 0:
+                        self.cue.apply_force(int(self.hit_power))
+                        self.mode = "game"
+                        self.cue.aim_cue(None)
+                        self.hit_power = ""
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     cursor_pos = pg.mouse.get_pos()
                     if self.mode == "game":
                         current_ball: Ball = self.get_ball_by_coord(Vector(cursor_pos[0], cursor_pos[1]))
                         if current_ball != None:
-                            # current_ball.velocity = Vector(-5, 0)
                             self.cue.aim_cue(current_ball)
-                    else:
+                            self.mode = "aimed"
+                    elif self.mode == "create":
                         ball = Ball(self.screen, Vector(cursor_pos[0], cursor_pos[1]))
-                        self.balls.append(ball)
+                        self.balls.append(ball)                
 
     def get_ball_by_coord(self, position: Vector):
         for ball in self.balls:
